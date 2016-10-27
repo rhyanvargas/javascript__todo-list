8b866ff557c5979a9b3989bb5535f146cc360be6
Rhyan Vargas <rhyanv1@gmail.com>
2016-10-26 21:09:35 -0400

    add eventlistener to javascript

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,15 @@
-document.getElementbyId("add");
+
+var addButton = document.getElementById('add');
+var itemInput = document.getElementById('item').value;
+
+// User clicked on the add button
+// If there is any text inside the item field, add that text to the todo list
+addButton.addEventListener('click',()=> {
+
+  // If an inputted item exists
+  if(itemInput){
+    
+  }
+
+    // If the item is already there, add the item to the list
+});
